5d7b669cf0694bd9d3b9feec59c41b703aef19fd
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-03-29 17:40:20 +0200

    Update run

--- run.py
+++ run.py
@@ -15,40 +15,52 @@ log.setLevel(logging.INFO)
 directory = os.path.dirname(os.path.realpath(__file__))
 
 
+def run_one(directory):
+    """
+
+    :param str directory:
+    """
+    if not os.path.isdir(directory):
+        return
+
+    if not os.path.exists(os.path.join(directory, 'config.yaml')):
+        return  # Skip non-INDRA machine directories
+
+    log.info('running in %s', directory)
+    # Use config=None so it looks in the subdirectory
+    run_with_search_helper(directory, config=None)
+
+    default_config_fname = os.path.join(directory, 'config.yaml')
+    config = get_config(default_config_fname)
+    ndex_cred = get_ndex_cred(config)
+    name = ndex_cred.get('name') if ndex_cred is not None else None
+
+    model = load_model(directory)
+    stmts = model.get_statements()
+
+    if not stmts:
+        log.warning('no statements')
+        return
+
+    # Output CX
+    if name:
+        cx_str = assemble_cx(stmts, name)
+        with open(os.path.join(directory, 'output.cx'), 'w') as file:
+            print(cx_str, file=file)
+
+    # Output BEL gpickle
+    bel_graph = pybel.from_indra_statements(stmts)
+    pybel.to_pickle(bel_graph, os.path.join(directory, 'output.gpickle'))
+
+    return True
+
+
 def main():
     """Runs the INDRA machine on all relevant subdirectories"""
     log.info('beginning INDRA machine runner')
 
     for subdirectory in sorted(os.listdir(directory)):
-        absolute_subdirectory = os.path.join(directory, subdirectory)
-
-        if not os.path.isdir(absolute_subdirectory):
-            continue
-
-        if not os.path.exists(os.path.join(absolute_subdirectory, 'config.yaml')):
-            continue  # Skip non-INDRA machine directories
-
-        log.info('running in %s', absolute_subdirectory)
-        # Use config=None so it looks in the subdirectory
-        run_with_search_helper(absolute_subdirectory, config=None)
-
-        default_config_fname = os.path.join(absolute_subdirectory, 'config.yaml')
-        config = get_config(default_config_fname)
-        ndex_cred = get_ndex_cred(config)
-        name = ndex_cred.get('name') if ndex_cred is not None else None
-
-        model = load_model(absolute_subdirectory)
-        stmts = model.get_statements()
-
-        # Output CX
-        if name:
-            cx_str = assemble_cx(stmts, name)
-            with open(os.path.join(absolute_subdirectory, 'output.cx'), 'w') as file:
-                print(cx_str, file=file)
-
-        # Output BEL gpickle
-        bel_graph = pybel.from_indra_statements(stmts)
-        pybel.to_pickle(bel_graph, os.path.join(absolute_subdirectory, 'output.gpickle'))
+        run_one(os.path.join(directory, subdirectory))
 
 
 if __name__ == '__main__':
